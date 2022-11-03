f10f34f4a907403e62f225a2222a478f5ed92667
jossySaragih <61966720+josray10@users.noreply.github.com>
2022-11-03 21:17:11 +0700

    Task_2(Query HTML) and Task_3 (DOM) Pemweb RC

---
+++ task_2/action.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informasi Produk - BuyBliPedia</title>
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+    <h1>BuyBliPedia</h1>
+    <div class="layoutproduk">
+        <h2>Informasi Detail Produk</h2>
+        <div class="box">
+            <img src="src/product1.jpg" alt="Logo-Patch Varsity Jacket">
+        </div>
+        <div class="informasi">
+            <h4>Off-White Logo-Patch Varsity Jacket</h4>
+            <p >ID: 001</p>
+            <p>Harga:<s> Rp. 82.165.850</s> Rp. 70.000.000</p>
+            <br>
+            <p>Logo-patch varsity jacket from OFF-WHITE featuring black, wool blend, stripe detailing, stand-up collar, front button fastening, long sleeves and logo patch.</p>
+        </div>
+        
+    </div>
+
+    <div class="layoutproduk">
+        <h2>Informasi Detail Produk</h2>
+        <div class="box">
+            <img src="src/product2.jpg" alt="Logo-Patch Varsity Jacket" height="269">
+        </div>
+        <div class="informasi">
+            <h4>Hoodie Essentials Reflect In The Dark Polar Fleece</h4>
+            <p >ID: 002</p>
+            <p>Harga: Rp. 950.000</p>
+            <br>
+            <p>BLACK/MULTICOLOR/MULTICOLOR</p>
+        </div>
+        
+    </div>
+
+    <div class="layoutproduk">
+        <h2>Informasi Detail Produk</h2>
+        <div class="box">
+            <img src="src/product3.jpg" alt="Logo-Patch Varsity Jacket" height="270">
+        </div>
+        <div class="informasi">
+            <h4>Off-White Logo-Patch Varsity Jacket</h4>
+            <p >ID: 003</p>
+            <p>Harga: Rp. 899.000</p>
+            <br>
+            <p>Jaket kemeja dengan potongan santai untuk gaya kasual. Dengan saku praktis dan serbaguna.</p>
+        </div>
+</body>
+</html>
